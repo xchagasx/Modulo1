dadd4c1d19272ad70a18f34709ea5ebc3d7260d3
xchagasx <xchagasx@yahoo.com.br>
2022-04-11 13:51:01 -0300

    Ex day 3...

---
+++ Bloco4/Exercises/trybe-exercicios/Fundamentos/bloco-4-introducao-a-javasript-e-logica-de-programcao/dia-3-javascript-logica-de-programacao-e-algoritmos/b4d3ex.js
@@ -0,0 +1,20 @@
+// O fatorial é representado pelo sinal !
+// 4! = 4 x 3 x 2 x 1 = 24
+
+// 10! = 10 x 9 x 8 x 7 x 6 x 5 x 4 x 3 x 2 x 1 = 3628800
+//
+//let fat = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1];
+//let mult = [];
+//    for(let i = 1; i < fat.length; i += 1){
+//        mult += fat.length[i];
+//    }       console.log();
+//
+
+
+//2- Agora, desenvolva um algoritmo que é capaz de inverter uma palavra. Por exemplo, a palavra "banana" seria invertida para "ananab". Utilize a string abaixo como exemplo, depois troque por outras para verificar se seu algoritmo está funcionando corretamente.
+
+let word = 'tryber';
+
+    for(let i = 0; i > word.length; i += 1){
+        console.log(word[i]);
+    }
