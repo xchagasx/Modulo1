6eb05680cc8a98f6106a5c6251b320b4b75892a9
xchagasx <xchagasx@yahoo.com.br>
2022-04-28 06:49:50 -0300

    finish style

--- Bloco6/fundamentos/bloco-6-html-e-css-forms-flexbox-e-responsivo/dia-1-html-css-forms/script.js
+++ Bloco6/fundamentos/bloco-6-html-e-css-forms-flexbox-e-responsivo/dia-1-html-css-forms/script.js
@@ -1,24 +1,30 @@
-const btn = document.querySelector('#send');
-const form = document.querySelector('#form');
-const write = document.querySelector('#write');
+function prevente() {
 
-function pValue(event){
+  const btn = document.querySelector('#send');
+  const form = document.querySelector('#form');
+  const write = document.querySelector('#write');
+  
+  function pValue(event){
     event.preventDefault();
     console.log(write.value);
-}
-btn.addEventListener('click', pValue);
-
-const boxConc2 = document.querySelector("#conc2");
-function enable() {
-  if (boxConc2.checked) {
-    btn.disabled = false;
-  } else {
-    btn.disabled = true;
   }
+  btn.addEventListener('click', pValue);
 }
-boxConc2.addEventListener('change', enable);
+
+function habilit() {
+  const btn = document.querySelector('#send');
+  const boxConc2 = document.querySelector("#conc2");
+  function enable() {
+    if (boxConc2.checked) {
+      btn.disabled = false;
+    } else {
+      btn.disabled = true;
+    }
+  }
+  boxConc2.addEventListener('change', enable);
+}
 
 window.onload = function() {
-enable();
-pValue();
+prevente();
+habilit();
 };
